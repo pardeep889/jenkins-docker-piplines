6fa76755d9f05f029a9cec8dec50eb68eb94e1e7
pardeep889 <pardeep889@hotmail.com>
2020-01-02 13:38:46 +0530

    docker file add

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 app.get("/", function (req, res) {
  res.send("hello world").status(200);
 });
-//listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
+//listen to port 8000 by default
+app.listen(process.env.PORT || 8000);
  
 module.exports = app;
