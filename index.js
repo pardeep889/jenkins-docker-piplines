f4a05ca4b911969aeb86c96a39c3004623b7f7f5
pardeep889 <pardeep889@hotmail.com>
2020-01-03 17:48:50 +0530

    Working on infa

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-//importing node framework
-var express = require("express");
- 
+var express = require("express"); 
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
  res.send("hello world").status(200); //return hello world
 });
 
-// I am new thing I should be in the master hahahaha
 //listen to port 8000 by default
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`App is running on ${port}`));
