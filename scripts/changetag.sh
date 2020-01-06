69d637ac1b6b52ad3747b482063b6e6913e8cee4
pardeep889 <pardeep889@hotmail.com>
2020-01-06 18:13:47 +0530

    infa change

--- scripts/changetag.sh
+++ scripts/changetag.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed "s/tagVersion/$1/g" infrastructure/pods.yaml > infrastructure/pods-live.yaml
+sed "s/tagVersion/$1/g" infrastructure/r-deployments.yaml > infrastructure/deployments.yaml
